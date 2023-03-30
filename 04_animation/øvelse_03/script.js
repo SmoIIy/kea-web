5d1dbc81248023223ca934153847170a653b2c82
SmoIIy <91464612+SmoIIy@users.noreply.github.com>
2023-03-30 14:32:10 +0200

    Animation øvelse 3

---
+++ 04_animation/øvelse_03/script.js
@@ -0,0 +1,11 @@
+function animationFunction() {
+    const element = document.getElementById("box");
+    element.classList.add("animation")
+    console.log("fuck you");
+}
+
+function animationFunctionStop() {
+    const element = document.getElementById("box");
+    element.classList.remove("animation")
+    console.log("ej okay");
+}
