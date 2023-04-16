6742daf5a6bf0a14d3ea230b842e890547442859
SmoIIy <91464612+SmoIIy@users.noreply.github.com>
2023-04-16 15:13:47 +0200

    Animation

--- 04_animation/effekt_5_6_7_8/effekt_5_6_7_8/drej_hop.js
+++ 04_animation/effekt_5_6_7_8/effekt_5_6_7_8/drej_hop.js
@@ -2,11 +2,15 @@ const blue = document.querySelector("#blue_container");
 const red = document.querySelector("#red_container");
 
 window.addEventListener("load", drej);
-red.addEventListener("animationend", hop)
+red.addEventListener("animationiteration", hop);
+blue.addEventListener("animationend", removeHop);
 
 function drej() {
     red.classList.add("drej");
 }
 function hop() {
     blue.classList.add("hop");
+}
+function removeHop(){
+    blue.classList.remove("hop");
 }
