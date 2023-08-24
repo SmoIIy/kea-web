f2d77573f123c0baa64b0d0c925a4a4a5f9dd0e0
SmoIIy <91464612+SmoIIy@users.noreply.github.com>
2023-08-24 10:28:02 +0200

    Jaer

---
+++ 07_web/HTML_Struktur_1/script.js
@@ -0,0 +1,10 @@
+const title = document.querySelector("h1");
+const articleCont = document.querySelector(".articlecontainer");
+const art = document.querySelector("article");
+const pic = document.querySelector("article img");
+
+console.log(title);
+console.log(articleCont);
+console.log(art);
+console.log(pic);
+
