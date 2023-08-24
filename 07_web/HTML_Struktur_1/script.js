e1ad79c912b258f59a3a8666d19568fd791238b2
SmoIIy <91464612+SmoIIy@users.noreply.github.com>
2023-08-24 10:56:00 +0200

    sm up

--- 07_web/HTML_Struktur_1/script.js
+++ 07_web/HTML_Struktur_1/script.js
@@ -2,9 +2,16 @@ const title = document.querySelector("h1");
 const articleCont = document.querySelector(".articlecontainer");
 const art = document.querySelector("article");
 const pic = document.querySelector("article img");
+const undertitel = document.querySelector("main h2")
+
+
 
 console.log(title);
 console.log(articleCont);
 console.log(art);
 console.log(pic);
+
+
+title.innerHTML = "Hvad er det der foregår";
+undertitel.innerHTML = "Oh my god, please"
 
